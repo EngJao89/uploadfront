05f7b75403d06d230824c98172aa7ac308a79f37
EngJao89 <eng_joao@outlook.com>
2022-04-03 16:23:37 -0300

    Closes 03: "Implementa lista de uploads."

---
+++ src/components/FileList/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
+
+import { Container, FileInfo, Preview} from './styles';
+
+const FileList = () => (
+  <Container>
+    <li>
+      <FileInfo>
+        <Preview src="https://avatars2.githubusercontent.com/u/2254731?v=3&s=460" />
+        <div>
+          <strong>react-file-upload</strong>
+          <span>417.5KB <button onClick={() => {}}>Excluir</button></span>
+        </div>
+      </FileInfo>
+      <div>
+        <CircularProgressbar 
+          styles={{
+            root: { width: 24 },
+            path: { stroke: '#7159c1' }
+          }}
+          strokeWidth={10}
+          percentage={60}
+        />
+
+        <a 
+          href="https://avatars2.githubusercontent.com/u/2254731?v=3&s=460" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+        </a> 
+        <MdCheckCircle size={24} color="#78e5d5" />
+        <MdError size={24} color="#e57878" />  
+      </div>
+    </li>
+  </Container>
+);
+
+export default FileList;
