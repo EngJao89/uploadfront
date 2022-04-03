2be3a4cae10a51bafe7757962f62e03e0944fe75
EngJao89 <eng_joao@outlook.com>
2022-04-03 18:13:53 -0300

    Closes 04: "Implementa funções do client."

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -4,37 +4,43 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview} from './styles';
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="https://avatars2.githubusercontent.com/u/2254731?v=3&s=460" />
-        <div>
-          <strong>react-file-upload</strong>
-          <span>417.5KB <button onClick={() => {}}>Excluir</button></span>
-        </div>
-      </FileInfo>
-      <div>
-        <CircularProgressbar 
-          styles={{
-            root: { width: 24 },
-            path: { stroke: '#7159c1' }
-          }}
-          strokeWidth={10}
-          percentage={60}
-        />
-
-        <a 
-          href="https://avatars2.githubusercontent.com/u/2254731?v=3&s=460" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-        >
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a> 
-        <MdCheckCircle size={24} color="#78e5d5" />
-        <MdError size={24} color="#e57878" />  
-      </div>
-    </li>
+    {files.map(uploadedFile => (
+      <li key={uploadedFile.id}>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>{uploadedFile.readableSize} <button onClick={() => onDelete(uploadedFile.id)}>Excluir</button></span>
+          </div>
+          </FileInfo>
+          <div>
+            {!uploadedFile.uploaded && !uploadedFile.error && (
+              <CircularProgressbar 
+                styles={{
+                  root: { width: 24 },
+                  path: { stroke: '#7159c1' }
+                }}
+                strokeWidth={10}
+                percentage={uploadedFile.progress}
+              />
+            )}
+    
+            {uploadedFile.url && (
+              <a 
+                href={uploadedFile.url} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+              >
+                <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+              </a> 
+            )}
+            {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+            {uploadedFile.error && <MdError size={24} color="#e57878" /> }
+          </div>
+        </li>
+    ))}
   </Container>
 );
 
